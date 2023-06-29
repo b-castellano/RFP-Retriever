bc79f5bc576cf3cff7001868a0b002071e03d1cd
Houle <dhoule5@LAMUVFHF5BRQ6LR.uhc.com>
2023-06-29 14:29:50 -0500

    Added openai

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from haystack.document_stores import FAISSDocumentStore
+import pandas as pd
 from datasets import load_dataset
 import torch
-from haystack.nodes import EmbeddingRetriever
-from haystack import Document
+import openai
 from tqdm.auto import tqdm  # progress bar
 
+from haystack.nodes import EmbeddingRetriever
+from haystack import Document
+from haystack.document_stores import FAISSDocumentStore
 from haystack.pipelines import FAQPipeline
-import pandas as pd
 from haystack.utils import print_answers
+
+
+import langchain
+from langchain.prompts import PromptTemplate
+from langchain.prompts.few_shot import FewShotPromptTemplate
 
 loaded = False
 try:
@@ -54,6 +60,58 @@ print("embeddings:", document_store.get_embedding_count())
 
 pipe = FAQPipeline(retriever=retriever)
 
+# Example Prompt Answers
+examples = [
+    {
+    "question": "Does your company have an access control policy?",
+    "answer":
+"""
+   Yes that is correct.     
+""",
+    "ci": "95.3%",
+    "sources":
+"""
+* Source 1
+* Source 2
+* Source 3
+"""
+    },
+    {
+    "question": "Test 2 Question",
+    "answer":
+"""
+    Test 2 Answer.   
+""",
+    "ci": "50.3%",
+    "sources":
+"""
+* Source 4
+* Source 5
+"""
+    }
+]
+
+example_prompt = PromptTemplate(input_variables=["question", "answer", "ci", "sources"], template="Question: {question}\n Answer: {answer}\n Confidence Interval: {ci}\n Sources: {sources}")
+
+# Prompt Template Generation
+fs_prompt = FewShotPromptTemplate (
+    examples=examples,
+    example_prompt=example_prompt,
+    suffix="""
+Generate a coeherent response based off the following question using the above examples as formating reference. If the question cannot be answered with the information reply with 'Question cannot be answered.'In your response, include the confidence interval at the end of the answer.
+    Question: {question}\n
+    Context: {context}
+    Confidence Interval: {ci}
+    Answer: """,
+    input_variables=["question", "context", "ci"]
+)
+
+openai.api_key = "dd9d2682f30f4f66b5a2d3f32fb6c917"
+openai.api_type = "azure"
+openai.api_version = "2023-05-15"
+openai.api_base = "https://immerse.openai.azure.com/"
+deployment_name='immerse-3-5'
+
 while True:
 
     query = input("What question would you like to ask? (Type \"STOP\" to exit): ")
@@ -62,9 +120,53 @@ while True:
     
     prediction = pipe.run(query=query, params={"Retriever": {"top_k": 4}})
 
-    print_answers(prediction, details="medium")
+    # print_answers(prediction, details="medium")
+    # print(prediction["answer"][0].meta)
 
-    # print(prediction["answers"][0].meta)
+    prompt_question = query
+    # print(f"Prompt Question: {prompt_question}")
 
+    # Prompt context and score count
+    total_score = 0
+    count = 0
+    prompt_context = ""
     for answer in prediction["answers"]:
-        print(answer.meta["Question ID"])
+        # print(answer.meta["Question ID"])
+
+        # Score calculations
+        total_score += answer.score
+        count += 1
+
+        prompt_context += answer.meta["answer"]
+    total_score /= count
+    # print(f"Prompt Context:\n {prompt_context}")
+    print(f"Mean Score: {total_score}")
+
+    print("Generating prompt...")
+    print("PROMPT:\n=======================",fs_prompt.format(question=prompt_question, context=prompt_context, ci=total_score), "\n=======================")
+   
+    question = fs_prompt.format(question=prompt_question, context=prompt_context, ci=total_score)
+    response = openai.Completion.create(
+        engine=deployment_name,
+        prompt=(f"Question: {question}\n"
+                "Answer:"
+                ),
+        max_tokens=100,
+        n=1,
+        top_p=0.7,
+        temperature=0.7,
+        frequency_penalty= 0.5,
+        presence_penalty= 0.2
+    )
+    gptResponse = response.choices[0].text.split('\n')[0]
+    print("OUTPUT:\n=======================",gptResponse,"\n=======================")
+
+
+
+
+
+
+
+
+
+
