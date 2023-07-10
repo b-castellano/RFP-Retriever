aa54ec72027575192b468977119009cbb68dbb0c
hcordes_uhg <hunter_cordes@optum.com>
2023-07-10 11:23:35 -0500

    PreMerge

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from datasets import load_dataset
 import pandas as pd
 import torch
 import openai
-import os
-import random
+from tqdm.auto import tqdm  # progress bar
+import traceback
 import re
 
 # Haystack
@@ -22,90 +22,6 @@ from langchain.prompts import PromptTemplate
 from langchain.prompts.few_shot import FewShotPromptTemplate
 from langchain.output_parsers import CommaSeparatedListOutputParser
 
-# Warning filter
-warnings.filterwarnings('ignore', "TypedStorage is deprecated", UserWarning)
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-## TEMPLATE STORAGE
-# FewShotPrompt Template (WORST) --> To much information?
-examples = [
-    {
-    "question": "Does your company have an access control policy?",
-    "answer":
-"""
-   Yes, that is correct.     
-""",
-    "ci": "95.3%",
-    "sources":
-"""
-* Source 1
-* Source 2
-* Source 3
-"""
-    },
-    {
-    "question": "Test 2 Question",
-    "answer":
-"""
-    Test 2 answer.   
-""",
-    "ci": "50.3%",
-    "sources":
-"""
-* Source 1
-* Source 2
-"""
-    }
-]
-example_prompt = PromptTemplate(input_variables=["question", "answer", "ci", "sources"], template="Question: {question}\n Answer: {answer}\n Confidence Interval: {ci}\n Sources: {sources}")
-
-fs_template = FewShotPromptTemplate (
-    examples=examples,
-    example_prompt=example_prompt,
-    suffix="""
-Generate a coeherent response based off the following question using the above examples as formating reference. If the question cannot be answered with the information reply with 'Question cannot be answered.'
-    Question: {question}\n
-Please use information from the following context documents in the response and list the question IDs as sources in bullet points.
-    Context: {context}
-Also include the confience interval at the end of the answer.
-    Confidence Interval: {ci}
-    Answer:""",
-    input_variables=["question", "context", "ci"]
-)
-
-# Normal Template --> Too simple of answer, sometimes list confidence intervals, lists sources.
-template = """Give a coherent response to the question based on the context below then include the confidence score and question IDs. Fill in the required information in the empty spaces below.
-
-Question: {question}
-Context: {context}
-Confidence Score: {ci}
-Question IDs: {ID}
-
-Provde an answer then put confidence score after and put all question IDs as sources from the above information.
-
-Output:
-"""
-
-gpt_template = PromptTemplate (
-    input_variables=["context","question","ci","ID"],
-    template=template
-)
-
-# Simple Template --> Simple answer
-template_simple = """Give a coherent and thorough response to the question based on the context below.
-
-Question: {question}
-
-Context: {context}
-
-Answer:
-"""
-
-gpt_template_simple = PromptTemplate (
-    input_variables=["question","context"],
-    template=template_simple
-)
-
 # Dylan prefix tempalte (BEST) --> Outputs solid answer, lists sources, gives relatively accurate confidence interval.
 template_dylan = """You are an assistant for the Information Security department of an enterprise designed to answer security questions in a professional manner.
 Provided is the original question and some context consisting of a sequence of answers in the form of 'question ID and answer'.
@@ -119,67 +35,6 @@ Context: {context}
 gpt_template_dylan = PromptTemplate (
     input_variables=["question", "context"],
     template=template_dylan
-)
-
-# JSON Template ---> Does not work at all
-template_json = """"You are an assistant for the Information Security department of an enterprise designed to answer security questions in a professional manner. 
-Provided is the original question and some context consisting of a sequence of answers in the form of 'question ID, confidence score, and answer'. 
-Use the answers within the context to formulate a concise response.
-You must add at the end of the response the referenced question IDs in the form of a list."
-
-Question: {question}
-
-Context: {context}
-
-Answer in a JSON like this:
-"Answer": ...,
-"Sources": ...
-"""
-
-gpt_template_json = PromptTemplate (
-    input_variables=["question", "context"],
-    template=template_json
-)
-
-
-# Output Parser Template -> Only lists sources
-output_parser = CommaSeparatedListOutputParser()
-format_instructions = output_parser.get_format_instructions()
-
-list_template = """"You are an assistant for the Information Security department of an enterprise designed to answer security questions in a professional manner. 
-Provided is the original question and some context consisting of a sequence of answers in the form of 'question ID, confidence score, and answer'. 
-Use the answers within the context to formulate a concise response. 
-At the end of the response give the referenced question IDs in the form of a list."
-Question: {question}
-
-Context: {context}
-
-Make a list of the referenced question IDs
-List:\n {format_instructions}
-"""
-
-gpt_list_template = PromptTemplate (
-    input_variables=["question", "context"],
-    template=list_template,
-    partial_variables={"format_instructions": format_instructions}
-)
-
-# Modified FewShotTemplate --> Issues with too much text.
-fs_template_modified = FewShotPromptTemplate (
-    examples=examples,
-    example_prompt=example_prompt,
-    suffix="""
-    You are an assistant for the Information Security department of an enterprise designed to answer security questions in a professional manner.
-    Use the examples above as a reference for formating.
-    Provided is the original question and some context consisting of a sequence of answers in the form of 'question ID, confidence score, and answer'.
-    Use the answers within the context to formulate a concise response.
-    At the end of the response give the referenced question IDs in the form of a list as well as the average confidence score for the referenced context portions.
-    "
-    Question: {question}
-
-    Context: {context}
-    """,
-    input_variables=["question", "context"]
 )
 
 def init_store():
@@ -220,130 +75,63 @@ def write_docs(document_store, retriever):
     print("docs added:", document_store.get_document_count())
     print("docs embedded:", document_store.get_embedding_count())
 
-def init_pipe(retriever):
-    return FAQPipeline(retriever=retriever)
 
-def query_faiss(query, pipe):
-    return pipe.run(query=query, params={"Retriever": {"top_k": 4}})
+pipe = FAQPipeline(retriever=retriever)
 
-def create_prompt(query, prediction):
-    prompt = gpt_template_dylan
-    IDs = {}
 
-    # Create context
-    prompt_context = ""
-    prompt_ids = ""
-    avgscore = 0
-    count = 0
-    for answer in prediction["answers"]:
-        id = answer.meta["question ID"]
-        prompt_context = re.sub("[\[\]']","",answer.meta["answer"])
-        score = answer.score
-        prompt_context += "Question ID: {ID}\n Content: {content}\n".format(ID=id, content=prompt_context)
-        prompt_ids += "{ID}\n".format(ID=id)
+openai.api_key = "dd9d2682f30f4f66b5a2d3f32fb6c917"
+openai.api_type = "azure"
+openai.api_version = "2023-05-15"
+openai.api_base = "https://immerse.openai.azure.com/"
+deployment_name = 'immerse-3-5'
 
-        IDs[id] = score
+# while True:
+query = "Has your organization implemented data loss prevention (DLP) to detect potential unauthorized access, use, or disclosure of client data?"
+# query = input("What question would you like to ask? (Type \"STOP\" to exit): ")
+# if query == "STOP":
+#     break
+
+prediction = pipe.run(query=query, params={"Retriever": {"top_k": 4}})
+
+# Create prompt template
+prompt = PromptTemplate(input_variables=["prefix", "question", "context"],
+                        template="{prefix}\nQuestion: {question}\n Context: {context}\n")
+
+# Provide instructions/prefix
+prefix = "You are an assistant for the Information Security department of an enterprise designed to answer security questions in a professional manner. Provided is the original question and some context consisting of a sequence of answers in the form of 'question ID, confidence score, and answer'. Use the answers within the context to formulate your response in under two hundred words. In addition, list the referenced question IDs of the answers you referenced at the end of your response."
+
+# Create context
+context = ""
+avgscore = 0
+count = 0
+for answer in prediction["answers"]:
+    if (answer.score > .7):
+        context += "Question ID: {ID}, Content: {content}\n".format(
+            ID=answer.meta["question ID"], content=answer.meta["answer"])
         avgscore += answer.score
-        count += 1
-    avgscore /= count
+        count+=1
+avgscore /= count   # convert total score to avg
+avgscore *= 100      # convert from decimal to percentage
 
-    print("Generating prompt...")
-    return prompt.format(question=query, context=prompt_context), IDs
+# Generate Prompt
+print("Generating prompt...")
+prompt = prompt.format(prefix=prefix, question=query, context=context)
+print("PROMPT:", prompt)
 
-def init_gpt():
-    openai.api_key = "dd9d2682f30f4f66b5a2d3f32fb6c917"
-    openai.api_type = "azure"
-    openai.api_version = "2023-06-01-preview"
-    openai.api_base = "https://immerse.openai.azure.com/"
-    
-def call_gpt(prompt):
-    deployment_name = 'immerse-3-5'
-    response = openai.Completion.create(
-        engine=deployment_name,
-        prompt=(prompt),
-        max_tokens=1000,
-        n=1,
-        top_p=0.7,
-        temperature=0.2,
-        frequency_penalty=0.5,
-        presence_penalty=0.2
-    )
-    return response.choices[0].text.split('\n')[0]
+# Call openai API
+response = openai.Completion.create(
+    engine=deployment_name,
+    prompt=(f"Question: {prompt}\n"
+            "Answer:"
+            ),
+    max_tokens=300,
+    n=1,
+    top_p=0.7,
+    temperature=0.3,
+    frequency_penalty=0.5,
+    presence_penalty=0.2
+)
 
-def compute_average(gpt, dict):
-    avgscore = 0
-    try:
-        txt = re.search("\[(.*)\]", gpt)
-        txt_split = re.split(",\s?", txt.group(1))
-        count = 0
-        for word in txt_split:
-            avgscore += dict[word]
-            count += 1
-        avgscore /= count
-    except:
-        print("Cannot Find")
-    return avgscore
+gptResponse = response.choices[0].text.split('\n')[0]
 
-def main():
-    # Initialize document store
-    loaded = False
-    document_store, loaded = init_store()
-
-    # Initialize retriever
-    retriever = init_retriever(document_store)
-    
-    # Check if docs are stored
-    if not loaded:
-        write_docs(document_store, retriever)
-
-    # Initialize pipeline
-    pipe = init_pipe(retriever)
-
-    # Initialize gpt bot
-    init_gpt()
-
-    while True:
-        #query = input("What question would you like to ask? (Type \"STOP\" to exit): ")
-        #if query == "STOP":
-        #    break
-
-        # good_query = "Please describe how you secure data at rest."
-        # bad_query = "Are encryption keys managed and maintained?"
-        count = 100
-        file = open("Output_2.txt", "w")
-        for n in range(count):
-            df = pd.read_csv("qna.csv")
-            query = df["question"][1]
-            print(query)
-
-            #query = "Are encryption keys managed and maintained?"
-
-            # Get relavant answers from database
-            prediction = query_faiss(query, pipe)
-
-            # Construct the prompt and dictionary
-            prompt, dict = create_prompt(query, prediction)
-
-            # Get gpt output for prompt
-            gpt_output = call_gpt(prompt)
-
-            # Get avaerage confidence interval for relavant answers
-            avgscore = compute_average(gpt_output, dict)
-
-            # Calculate failure rate
-            fail_count = 0
-            if avgscore < 1:
-                fail_count += 1
-
-            print(f"Question: {query}\n Output: {gpt_output}\n Score: {avgscore}")
-
-            txt = (f"Question: {query}\n" + f"{gpt_output}\n" + f"{avgscore}\n")
-            file.write(txt)
-        file.close()
-
-        fail_count /= count
-        print(f"Fail Ratio: {fail_count}")
-        break
-
-if __name__ == "__main__": 
-    main()
+print(f"OUTPUT:\n======================={gptResponse}\nConfidence score: {avgscore}%\n=======================")
